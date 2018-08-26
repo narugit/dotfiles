07ff7370228f0870db6eb8c6a1ab6425a89b46ca
narugit <narusens@gmail.com>
2018-08-26 10:08:35 +0900

    add message

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo 'start linking...'
 ln -sf ~/Documents/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Documents/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/Documents/dotfiles/.bashrc ~/.bashrc
@@ -6,3 +7,4 @@ ln -sf ~/Documents/dotfiles/.inputrc ~/.inputrc
 
 ln -sf ~/Documents/dotfiles/dein/userconfig/plugins.toml ~/.vim/dein/userconfig/plugins.toml
 ln -sf ~/Documents/dotfiles/dein/userconfig/plugins_lazy.toml ~/.vim/dein/userconfig/plugins_lazy.toml
+echo 'done!'

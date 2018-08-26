c4268472750ebbc0c0262a0033e141301975cfe3
narugit <narusens@gmail.com>
2018-08-26 10:56:19 +0900

    add .gitconfig

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,7 @@ ln -sf ~/Documents/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Documents/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/Documents/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/Documents/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/Documents/dotfiles/.gitconfig ~/.gitconfig
 
 ln -sf ~/Documents/dotfiles/dein/userconfig/plugins.toml ~/.vim/dein/userconfig/plugins.toml
 ln -sf ~/Documents/dotfiles/dein/userconfig/plugins_lazy.toml ~/.vim/dein/userconfig/plugins_lazy.toml

de2e195e190725085f42cf678116c9844d83542e
narugit <narusens@gmail.com>
2018-09-09 22:33:56 +0900

    .gitconfig.localの追加/glogの色を見やすくした

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,6 +4,7 @@ ln -sf ~/Documents/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Documents/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/Documents/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/Documents/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/Documents/dotfiles/.gitconfig.local ~/.gitconfig.local
 ln -sf ~/Documents/dotfiles/.gitconfig ~/.gitconfig
 
 ln -sf ~/Documents/dotfiles/dein/userconfig/plugins.toml ~/.vim/dein/userconfig/plugins.toml

8226ffeb90f5625e3d70178fc75cfce49f2bdcd7
narugit <narusens@gmail.com>
2021-09-18 16:20:37 +0900

    Add /bin and /etc

---
+++ etc/zsh/.zsh.d/7_man.zsh
@@ -0,0 +1,22 @@
+#=#=#=
+# Set options for less
+#
+# **Features**
+#
+# * with color
+# * different color for bold, underline, standout font
+# * verbose status line message (show file name)
+#=#=
+
+# less options:
+export LESS='-iMRgj.5'
+
+# colored less
+export LESS_TERMCAP_mb=$(tput bold)                # begin blinking
+export LESS_TERMCAP_md=$(tput bold; tput setaf 4)  # begin bold (blue)
+export LESS_TERMCAP_me=$(tput sgr0)                # end mode
+export LESS_TERMCAP_se=$(tput sgr0)                # end standout-mode
+export LESS_TERMCAP_so=$(tput bold; tput setaf 1)  # begin standout-mode (red)
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)     # end underline
+export LESS_TERMCAP_us=$(tput smul; tput setaf 2)  # begin underline (green)
+

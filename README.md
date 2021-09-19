d6a6136661470bdbd5f0b0c25af5f90afa636240
narugit <narusens@gmail.com>
2021-09-19 09:22:50 +0900

    setup for vim

--- README.md
+++ README.md
@@ -2,78 +2,9 @@
 
 This is my dotfiles' settings.
 
-## Bash Setting
+## How to Install
+under construction
 
-### ~/mylib/
-
-- [tadashi-aikawa/pureline-inspired](https://github.com/tadashi-aikawa/pureline-inspired)
-
-#### Install
-
-    $ mkdir ~/mylib && cd ~/mylib
-
-    $ git clone https://github.com/tadashi-aikawa/pureline-inspired  
-### ~/.vim/colors/
-#### molokai
-- [tomasr/molokai](https://github.com/tomasr/molokai)
-
-##### Install
-
-    $ mkdir -p ~/.vim/colors && cd
-
-    $ git clone https://github.com/tomasr/molokai
-
-    $ mv ~/molokai/colors/molokai.vim ~/.vim/colors/
-
-    $ rm -rf ~/molokai
-    
-#### iceberg
-- [cocopon/iceberg](https://github.com/cocopon/iceberg.vim)
-
-##### Install
-
-    $ mkdir -p ~/.vim/colors && cd
-
-    $ git clone https://github.com/cocopon/iceberg.vim
-
-    $ mv ~/iceberg.vim/colors/iceberg.vim ~/.vim/colors/
-
-    $ rm -rf ~/iceberg.vim
-
-## Vim Setting
-
-### ~/.vim/pack/mypack/start/
-
-or
-
-### dein
-
-    $ mkdir -p ~/.vim/dein/userconfig
-
-    $ touch ~/.vim/dein/userconfig/plugins.toml ~/.vim/dein/userconfig/plugins_lazy.toml
-
-### nerdtree
-
-- [scrooloose/nerdtree](https://github.com/scrooloose/nerdtree)
-- [jistr/vim-nerdtree-tabs](https://github.com/jistr/vim-nerdtree-tabs)
-- [Xuyuanp/nerdtree-git-plugin](https://github.com/Xuyuanp/nerdtree-git-plugin)
-
-### markdown
-
-- [plasticboy/vim-markdown](https://github.com/plasticboy/vim-markdown)
-- [previm/previm](https://github.com/previm/previm)
-- [tyru/open-browser.vim](https://github.com/tyru/open-browser.vim)
-
-### ファイルアイコン
-
-- [ryanoasis/vim-devicons](https://github.com/ryanoasis/vim-devicons)
-
-#### 使用したフォント
-
-- [Cica 2.0.0](https://github.com/miiton/Cica)
-
-フォントをインストールしてBashのフォントに設定する
-
-## Other tools
-- [fzf](https://github.com/junegunn/fzf#using-git): official `.sh`
-- [bat](https://github.com/sharkdp/bat): `brew`
+```console
+$ bash -c "$(curl -fsSL https://raw.githubusercontent.com/narugit/dotfiles/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,99 @@
+#!/bin/bash -ex
+
+DOTFILES_DIR="${HOME}/dotfiles"
+
+case `uname` in
+  Darwin)
+    IS_OSX=true
+    IS_LINUX=false
+  ;;
+  Linux)
+    IS_LINUX=true
+    IS_OSX=false
+  ;;
+esac
+
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+title() {
+  echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+  echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+error() {
+  echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+  exit 1
+}
+
+warning() {
+  echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+}
+
+info() {
+  echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+}
+
+success() {
+  echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
+download_dotfiles() {
+  git clone https://github.com/narugit/dotfiles.git ${DOTFILES_DIR}
+}
+
+change_shell() {
+  # change shell to zsh
+  if ${IS_LINUX}; then
+    info "linux"
+    # Linux's default shell is bash, so change to zsh
+  fi
+}
+
+download_font() {
+  # download font from https://github.com/miiton/Cica
+}
+
+setup_vim() {
+  title "Setup vim"
+  
+  info "Downloading color scheme"
+  local VIM_COLOR_DIR="${HOME}/.vim/colors"
+  if [ ! -e "${VIM_COLOR_DIR}" ]; then
+    info "Creating directory for vim colors"
+    mkdir -p ${VIM_COLOR_DIR}
+  fi
+  curl -fsSL https://raw.githubusercontent.com/cocopon/iceberg.vim/master/colors/iceberg.vim -o "${VIM_COLOR_DIR}/iceberg.vim"
+
+  local VIM_DIR_DEST="${HOME}"
+  local VIM_DIR_SRC="${DOTFILES_DIR}/etc/vim"
+  info "Creating symlink for vim"
+  ln -snfv "${VIM_DIR_SRC}/.vimrc" "${VIM_DIR_DEST}/.vimrc"
+
+  local DEIN_PLUGINS_DIR_DEST="${HOME}/.vim/dein/userconfig"
+  local DEIN_PLUGINS_DIR_SRC="${VIM_DIR_SRC}/dein"
+  if [ ! -e "${DEIN_PLUGINS_DIR}" ]; then
+    info "Creating directory for dein plugins"
+    mkdir -p ${DEIN_PLUGINS_DIR}
+  fi
+  info "Creating symlink for dein plugins"
+  ln -snfv "${DEIN_PLUGINS_DIR_SRC}/plugins.toml" "${DEIN_PLUGINS_DIR}/plugins.toml"
+  ln -snfv "${DEIN_PLUGINS_DIR_SRC}/plugins_lazy.toml" "${DEIN_PLUGINS_DIR}/plugins_lazy.toml"
+}
+
+setup_dotfiles_sync_checker() {
+  # register tracker for dotfiles modification
+}
+
+download_dotfiles
+change_shell
+download_font
+setup_vim
+setup_dotfiles_sync_checker
+
+success "Install successful."

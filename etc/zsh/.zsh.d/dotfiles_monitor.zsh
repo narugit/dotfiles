fe2f7dc79bc552d6b5fabb80076e52f08cec5cc5
narugit <narusens@gmail.com>
2021-09-20 16:42:24 +0900

    [monitor] support active time

--- etc/zsh/.zsh.d/dotfiles_monitor.zsh
+++ etc/zsh/.zsh.d/dotfiles_monitor.zsh
@@ -3,6 +3,21 @@ DOTFILES_REMOTE_URL="https://github.com/narugit/dotfiles"
 
 source "${DOTFILES_DIR}/bin/log.sh"
 
+ACTIVE_HOUR_START=10
+ACTIVE_HOUR_END=17
+
+# rarity: one time in how many times to compare with remote's dotfiles.
+RARITY_MIN=1
+RARITY_ACTIVE_HOUR=10
+RARITY_NON_ACTIVE_HOUR=1
+
+generate_random() {
+  local min="$1"
+  local max="$2"
+  local random_num=$(awk -v min="$min" -v max="$max" 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+  echo "$random_num"
+}
+
 is_online() {
   local TEST_SERVER_URL="http://google.com"
   local RETRY_NUM=10
@@ -26,14 +41,53 @@ is_hash_same() {
   fi
 }
 
-if is_online; then
-  title "Comparing remote dotfiles"
-  if is_hash_same; then
-    info "Your local dotfiles is the latest!"
+is_active_hour() {
+  current_hour=$(date +"%H")
+  if [ ${ACTIVE_HOUR_START} -le ${current_hour} ] && [ ${current_hour} -lt ${ACTIVE_HOUR_END} ]; then
+    true
   else
-    warning "Your local dotfiles differs from remote dotfiles. Please check ${DOTFILES_REMOTE_URL}"
+    false
   fi
-else
-  warning "Offline. Please check your internet connection."
+}
+
+
+check_rarity() {
+  if [ ${RARITY_ACTIVE_HOUR} -lt ${RARITY_MIN} ]; then
+    error "RARITY_ACTIVE_HOUR should be greater than ${RARITY_MIN}."
+  fi
+  if [ ${RARITY_NON_ACTIVE_HOUR} -lt ${RARITY_MIN} ]; then
+    error "RARITY_NON_ACTIVE_HOUR should be greater than ${RARITY_MIN}."
+  fi
+
+  if is_active_hour; then
+    echo ${RARITY_ACTIVE_HOUR}
+  else
+    echo ${RARITY_NON_ACTIVE_HOUR}
+  fi
+}
+
+lottery_to_compare() {
+  rarity=$(check_rarity)
+  num=$(generate_random ${RARITY_MIN} $rarity)
+
+  if [ $num = ${RARITY_MIN} ]; then
+    true
+  else
+    false
+  fi
+}
+
+
+if lottery_to_compare; then
+  title "Comparing remote dotfiles"
+  if is_online; then
+    if is_hash_same; then
+      info "Your local dotfiles is the latest!"
+    else
+      warning "Your local dotfiles differs from remote dotfiles. Please check ${DOTFILES_REMOTE_URL}"
+    fi
+  else
+    warning "Offline. Please check your internet connection."
+  fi
 fi
 

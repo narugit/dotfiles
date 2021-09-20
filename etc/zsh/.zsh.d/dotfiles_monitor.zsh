68c3b4f32ba171f4e5423394a193a988ff145fdf
narugit <narusens@gmail.com>
2021-09-20 17:01:22 +0900

    [monitor] separate comparing to remote script

--- etc/zsh/.zsh.d/dotfiles_monitor.zsh
+++ etc/zsh/.zsh.d/dotfiles_monitor.zsh
@@ -1,6 +1,3 @@
-DOTFILES_DIR="${HOME}/dotfiles"
-DOTFILES_REMOTE_URL="https://github.com/narugit/dotfiles"
-
 source "${DOTFILES_DIR}/bin/log.sh"
 
 ACTIVE_HOUR_START=10
@@ -18,29 +15,6 @@ generate_random() {
   echo "$random_num"
 }
 
-is_online() {
-  local TEST_SERVER_URL="http://google.com"
-  local RETRY_NUM=10
-  local TIMEOUT_SEC=20
-  wget -q --tries="${RETRY_NUM}" --timeout="${TIMEOUT_SEC}" --spider "${TEST_SERVER_URL}"
-  if [ "$?" = 0 ]; then
-    true
-  else
-    false
-  fi
-}
-
-is_hash_same() {
-  local DOTFILES_DEFAULT_BRANCH=$(cd ${DOTFILES_DIR} && git remote show origin | sed -n '/HEAD branch/s/.*: //p')
-  local DOTFILES_REMOTE_HASH=$(cd ${DOTFILES_DIR} && git ls-remote origin HEAD | awk '{print $1}')
-  local DOTFILES_LOCAL_HASH=$(cd ${DOTFILES_DIR} && git rev-parse ${DOTFILES_DEFAULT_BRANCH})
-  if [ "${DOTFILES_LOCAL_HASH}" = "${DOTFILES_REMOTE_HASH}" ]; then
-    true
-  else
-    false
-  fi
-}
-
 is_active_hour() {
   current_hour=$(date +"%H")
   if [ ${ACTIVE_HOUR_START} -le ${current_hour} ] && [ ${current_hour} -lt ${ACTIVE_HOUR_END} ]; then
@@ -49,7 +23,6 @@ is_active_hour() {
     false
   fi
 }
-
 
 check_rarity() {
   if [ ${RARITY_ACTIVE_HOUR} -lt ${RARITY_MIN} ]; then
@@ -77,17 +50,7 @@ lottery_to_compare() {
   fi
 }
 
-
 if lottery_to_compare; then
-  title "Comparing remote dotfiles"
-  if is_online; then
-    if is_hash_same; then
-      info "Your local dotfiles is the latest!"
-    else
-      warning "Your local dotfiles differs from remote dotfiles. Please check ${DOTFILES_REMOTE_URL}"
-    fi
-  else
-    warning "Offline. Please check your internet connection."
-  fi
+  source "${DOTFILES_DIR}/bin/compare_remote.sh"
 fi
 

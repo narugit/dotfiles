2f63368151223d3f89e0a87ba752aa43b849a2cc
narugit <narusens@gmail.com>
2021-09-22 20:44:30 +0900

    [test] add linux docker environment

---
+++ test/linux/dockerrun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DOTFILES_LINUX_IMAGE="dotfiles_test_ubuntu_20.04"
+TEST_USER="devman"
+TEST_USER_PASS="devman"
+
+docker build -t "${DOTFILES_LINUX_IMAGE}" --build-arg TEST_USER=${TEST_USER} --build-arg TEST_USER_PASS=${TEST_USER_PASS} .
+docker run --rm -it "${DOTFILES_LINUX_IMAGE}":latest su - "${TEST_USER}"
+
